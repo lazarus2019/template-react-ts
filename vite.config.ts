501afca2293822a24ff0b1d1895c04608223c255
Thai Son <thaisonbk2020@gmail.com>
2024-11-21 08:31:47 +0700

    feat: init source via vite

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      include: '**/*.svg?react',
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles" as *;`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    host: true,
+  },
+});
